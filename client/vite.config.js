8f95af4b052604ab977b9ee6ecfddb2372b4afd1
Mauro Leonardi <mauro.leonardi@hotmail.com>
2024-07-07 16:18:55 +1000

    all in server 1.2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import api from '../shared/api.directory'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:8080'
+      '/api': api.host
     }
   },
   plugins: [react()],
--- shared/api.directory.js
+++ shared/api.directory.js
@@ -1,7 +1,7 @@
 const api = {
-    host:"",
+    //host:"",
     //host:"http://localhost:8080",
-    //host:"https://server-login-mern.onrender.com",
+    host:"https://server-login-mern.onrender.com",
 
     auth:{
       login:"/api/auth/login",
